30f4c6e887cebb328c6782b983cd1e17580e6430
sridhar02 <kattasridhar02@gmail.com>
2020-09-27 01:34:53 +0530

    clean up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import "./App.css";
+import Random from "./Components/Random_V2";
+import Tag from "./Components/Tag_V2";
+
+const App = () => (
+  <div>
+    <h1>Random GIF application</h1>
+    <div className="main-container">
+      <Random />
+      <Tag />
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
